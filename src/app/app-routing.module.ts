8cad8b4b8eeab8a0f305705df1fc0a050d1af3f3
= <eric.yammine@telecom-st-etienne.fr>
2021-02-18 15:07:51 +0100

    Adding base pages and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  },
 ];
 
 @NgModule({
